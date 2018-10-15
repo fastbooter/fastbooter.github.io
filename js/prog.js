615767a1a7223e25b395360ea1ceeb31509424da
fastbooter <36358231+fastbooter@users.noreply.github.com>
2018-10-15 16:57:20 +0500

    Update prog.js

--- js/prog.js
+++ js/prog.js
@@ -2,7 +2,7 @@
 var array = [];
 array[0]="<a href=\"https://vk.com/away.php?to=https%3A%2F%2Fmega.nz%2F%23%21wzYRgDyR%21l9CUoYXoAsQOcr23i0Tyaf0zhfvfnsjcDCCgCe5uE_8\">Всё для проверки устройства</a><br>"
 array[1]="<a href=\"https://vk.com/away.php?to=https%3A%2F%2Fmega.nz%2F%23%21wyhgWBpC%21fMcdrJhPEofsZHXBGUzT17Z5PVU55Z7EEYZFpiQcMnU\">Kaspersky</a>"
-array[2]="<a href=\"https://vk.com/away.php?to=https%3A%2F%2Fmega.nz%2F%23%21Yu5wlD4D%21LiQmKRHZpMKYshW1EtuPk7uS5pAuzM71QQLatz18Bq0\">Активация win10</a>"
+array[2]="<a href=\"https://mega.nz/#!l74jnCgR!R8Dm2TgBRS0vZfKUDqjkfSpw6MjZ5f3kt7UjCKDBaPY\">Активация win10</a>"
 array[3]="<a href=\"https://mega.nz/#!035ERBQS!QngZWHw2ZtmdDLn01DhNMkvfiQ8-k6rzvoTZLBQ4ZjQ\">Виктория HDD</a>"
 array[4]="<a href=\"https://mega.nz/#!xuBCAYYS!JMN5Qtbmkqys1DpsIW_hSOXw1HjnfKUXSRWuW4IlHE8\">ADW cleaner</a>"
 array[5]="<a href=\"https://mega.nz/#!Z3olwRLb!YihCXgXWBj4egROop9eiIL1_omlEQkIWXCq1YysYYgk\">Загрузка windows</a>"

53a738ff6e15d3bbb1ef039a8bffd517002bb71c
fastbooter <36358231+fastbooter@users.noreply.github.com>
2020-04-04 22:22:09 +0500

    Add files via upload

--- new/js/index.js
+++ new/js/index.js
@@ -128,7 +128,6 @@ function setWidth(){
 }
 
 
-
 $(".ripple").on("click",function(event){
 $(this).append("<span class='ripple-effect'>");
 $(this).find(".ripple-effect").css({
@@ -140,7 +139,30 @@ $(this).find(".ripple-effect").css({
    $(this).remove();
   });
 });
-$(document).ready(function() { 
+
+
+
+// $(document).ready(function() { 
+
+// 	(function ($) { 
+// 		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+		
+// 		$('.tab ul.tabs li a').click(function (g) { 
+// 			var tab = $(this).closest('.tab'), 
+// 				index = $(this).closest('li').index();
+			
+// 			tab.find('ul.tabs > li').removeClass('current');
+// 			$(this).closest('li').addClass('current');
+			
+// 			tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+// 			tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+			
+// 			g.preventDefault();
+// 		} );
+// 	})(jQuery);
+
+// });
+function tablets() { 
 
 	(function ($) { 
 		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
@@ -159,4 +181,4 @@ $(document).ready(function() {
 		} );
 	})(jQuery);
 
-});
+}
